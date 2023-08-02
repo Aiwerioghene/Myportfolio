fc6fd09efb916a9283946cf89f35518a4fc4ef6d
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-02 19:07:05 +0100

    commit : created new files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export {default as Navbar} from './components/Navbar/Navbar';
+export {default as Home} from './components/Home/Home';
+export {default as About} from './components/About/About';
+export {default as Projects} from './components/Projects/Projects';
+export {default as Contacts} from './components/Contacts/Contacts';
