f3479cb2168cb8c171e4968b0c5a8a5e5e3b99be
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-03 10:29:23 +0100

    work abeg

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,16 @@ import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+    <div className="logo">Logo</div>
+    <ul className="nav-links">
+      <li><a href="components/Home/Home.jsx">Home</a></li>
+      <li><a href="/">About</a></li>
+      <li><a href="/">Services</a></li>
+      <li><a href="/">Contact</a></li>
+    </ul>
+  </nav>
+  );
+};
 
 export default Navbar
