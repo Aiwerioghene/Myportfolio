58a2a1927f9d8102763079b24402c851bce7668c
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-03 20:51:23 +0100

    commit : reinitialized

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Navbar} from './components/Navbar/Navbar';
-export {default as Home} from './components/Home/Home';
-export {default as About} from './components/About/About';
-export {default as Projects} from './components/Projects/Projects';
-export {default as Contacts} from './components/Contacts/Contacts';
