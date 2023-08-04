f4a49078e56d3bc6c6f2c7102fd20437b810e1ff
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-04 02:27:11 +0100

    added assets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './navbar.css';
-
+import me from '/Users/adesuwa/Desktop/my-react-app/react-portfolio/src/assest/me.jpg'
+import git from '/Users/adesuwa/Desktop/my-react-app/react-portfolio/src/assest/git.jpg'
+import linkedin from '/Users/adesuwa/Desktop/my-react-app/react-portfolio/src/assest/linkedin.jpg'
+import twitter from '/Users/adesuwa/Desktop/my-react-app/react-portfolio/src/assest/twitter.jpg'
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
@@ -10,23 +13,22 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="logo">Logo</div>
+      <div className="logo"><img src={me} alt="me"/> </div>
       <div className={`nav-links ${showMenu ? 'show' : ''}`}>
         <ul>
           <li>
-            <a href="#home">Home</a>
+            <a href="#home"><img src={git} alt="git" /></a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#about"> <img src={twitter} alt="twitter" /></a>
           </li>
           <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
+            <a href="#services"><img src={linkedin} alt="linkedIn" /></a>
           </li>
         </ul>
+
       </div>
+
       <div className="burger" onClick={toggleMenu}>
         <div className={`line1 ${showMenu ? 'active' : ''}`}></div>
         <div className={`line2 ${showMenu ? 'active' : ''}`}></div>
