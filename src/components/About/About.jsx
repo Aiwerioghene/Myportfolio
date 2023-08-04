d00469f9b23972e57498dbd614e57f1969a4ea29
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-04 21:20:43 +0100

    add

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,12 @@ import './about.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='word'>
+      {/* <h1>Hey!</h1> */}
+      {/* <p>Hello!👋🏾, My name is Adesuwa - `AH-DAY-SUE-WA'. I am a Front-End Developer, with knowledge in JavaScript, CSS, HTML. I also utilize Bootstrap as my preferred CSS framework for web development.👩🏽‍💻
+
+I specialize in creating responsive and user-friendly websites, focusing on delivering high-quality and interactive user experiences. Alongside coding, I am open to learning new things, as it enables me to enhance my skills.</p> */}
+    </div>
   )
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,10 @@ import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='introduction'>
+      <h1 className='introduction_name'>Front-End Web Developer</h1>
+      <p className='introduction-p'>Adesuwa Aiwerioghene</p>
+    </div>
   )
 }
 
