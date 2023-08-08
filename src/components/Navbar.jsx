41427780e34b11385b6b28887f09adb9c9e6cb62
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-08 18:46:56 +0100

    Navbar done.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+      <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
         <h1 className='w-full text-2xl font-bold text-white m-4'>Contact Me.</h1>
         <ul className='uppercase p-4'>
           <li className='p-4 border-b border-gray-600 font-bold hover:text-yellow-700 '> <a href='https://github.com/daysuwaa' target='_blank' rel='noreferrer'>GitHub</a></li>
