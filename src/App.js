76b3eef5260c5edc49decb52f2e850e089fe4a23
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-08 10:49:21 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import About from './components/About';
+import Home from './components/Home';
+// import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar />
-      <About />
+      <Home />
       </>
 
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Typed from 'react-typed';
 
-const About = () => {
+const Home = () => {
   return (
     <div className='w-[100%] md:w-[1240px] h-[100vh] flex flex-col items-center justify-center'>
       <Typed
         className='md:text-4xl lg:text-5xl sm:text-3xl text-xl font-bold md:pl-4 pl-2 text-white text-center'
         strings={[
           'Aiwerioghene Adesuwa' ,
-          'I am a Front End Developer 👩🏽‍💻',
+          'I am a Front End Developer',
           'Welcome to my Portfolio! 🥰'
         ]} 
         typeSpeed={100} 
@@ -19,4 +19,4 @@ const About = () => {
   );
 }
 
-export default About;
+export default Home;
