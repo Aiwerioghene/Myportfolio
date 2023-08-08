36ce9bcef016238d7e4b1962a87e9bc2a2646b88
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-08 09:31:17 +0100

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Me from '../assest/me.jpg';
 
 const Navbar = () => {
+  
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className= 'flex justify-between items-center h-24 max-w-[1540px] mx-auto text-white'>
+      <img src={Me} className='w-[70px] pl-6' alt='logo' />
+      <button className='mr-8 bg-white text-black p-2 rounded-full'>Resume</button>
+     
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
