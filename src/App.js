fa308424d8c67abf3f4b80c46a8ebcd61fbf9083
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-08 09:00:14 +0100

    app .js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
+
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Home />
-      <div></div>
-      <About />
-      <Projects />
-      <Contact />
+      
       
     </div>
   );
