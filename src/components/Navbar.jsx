220da27931d291d72c5e5f3fa3d5a3436da3328f
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-08 10:03:57 +0100

    Navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Me from '../assest/me.jpg';
+import Resume from '../assest/Resume_Adesuwa.pdf'
+
 
 const Navbar = () => {
   
   return (
-    <div className= 'flex justify-between items-center h-24 max-w-[1540px] mx-auto text-white'>
+    <div className= 'flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
       <img src={Me} className='w-[70px] pl-6' alt='logo' />
-      <button className='mr-8 bg-white text-black p-2 rounded-full'>Resume</button>
+      <p className='mr-8 text-white p-2 w-[100px] font-bold hover:border-b-4 hover:border-yellow-500'> 
+      <a href={Resume} target='_blank' rel="noreferrer">Resume</a></p>
      
     </div>
   );
