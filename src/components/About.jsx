d023b326037fe9dbfb73d05d240c5f6779ea1ad2
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-08 12:57:24 +0100

    About done.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Backg from '../assest/background.jpeg';
 
 const About = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  const handleScroll = () => {
-    const position = window.pageYOffset;
-    setScrollPosition(position);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
-    <div className='absolute flex flex-col items-center  h-screen '>
-      <div
-        className='absolute inset-0 z-0'
-        style={{ transform: `translateY(-${scrollPosition * 0.3}px)` }}
-      >
-        <img src={Backg} alt='Background' className='w-full h-full object-cover' />
-      </div>
-      <div className='relative z-10 p-8 text-black font-bold text-center'>
+    <div className='h-screen bg-cover bg-center' style={{ backgroundImage: `url(${Backg})` }}>
+      <div className='p-8 text-black font-bold text-center'>
         <h1 className='text-4xl md:text-5xl lg:text-6xl mb-8 md:mt-8'>
           Welcome to My Portfolio!
         </h1>
@@ -34,7 +14,6 @@ const About = () => {
           Let's collaborate to bring your ideas to life and create web experiences that leave a lasting impression!
         </p>
       </div>
-      
     </div>
   );
 };
