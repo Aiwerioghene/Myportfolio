c15fe12c7567954233c855954d61e64c11b2008e
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-10 10:57:06 +0100

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='w-[100%]  h-[100vh] flex flex-col items-center justify-center'>
       <Typed
-        className='md:text-5xl lg:text-6xl sm:text-4xl text-2xl font-bold md:pl-4 pl-2 text-white text-center'
+        className='md:text-5xl lg:text-6xl sm:text-4xl text-2xl font-bold md:pl-4 pl-2 text-center font-serif'
         strings={[
           'Front End Developer',
           'Welcome to my Portfolio! 🥰'
