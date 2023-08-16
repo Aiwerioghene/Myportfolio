6746f6a646b46573157ea95dbae8a3de284c4662
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-16 21:01:48 +0100

    added to it

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import Vans from '../assest/vans.png';
 import Uber from '../assest/Uber.png';
 import Flickr from '../assest/flickr.png';
 import Asos from '../assest/asos.png';
+import LockLingo from '../assest/locklingo.png';
 
 const Projects = () => {
   return (
@@ -53,16 +54,16 @@ const Projects = () => {
             </div>
             <button className='bg-black text-white rounded-md font-medium my-6 mx-auto px-6 py-3'><a href='https://daysuwaa.github.io/Asos-Clone-/' target='_blank' rel="noreferrer">Check it out!</a></button>
         </div>
-        {/* <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-            <img className='w-120 mx-auto mt-[3rem] bg-transparent ' src={Uber}alt=''/>
-            <p className='text-center text-4xl font-bold'>Uber Clone</p>
+        <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+            <img className='w-120 mx-auto mt-[3rem] bg-transparent ' src={LockLingo}alt=''/>
+            <p className='text-center text-4xl font-bold'>Password Generator - Lock Lingo</p>
             <div className='text-ceneter font-medium'>
                 <p className='py-2 border-b mx-8 mt-8'>Technologies</p>
-                <p className='py-2 border-b mx-8'>HTML, CSS, Bootstrap</p>
+                <p className='py-2 border-b mx-8'>React JS and Tailwind CSS</p>
                
             </div>
             <button className='bg-black text-white  rounded-md font-medium my-6 mx-auto px-6 py-3'><a href='https://daysuwaa.github.io/Uber-clone/' target='_blank' rel="noreferrer">Check it out!</a></button>
-        </div> */}
+        </div>
         
         {/* <div className='w-full shadow-xl  bg-gray-100 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
             <img className='w-120 mx-auto mt-[3rem]' src={Flickr}alt=''/>
