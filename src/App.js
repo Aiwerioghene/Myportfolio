b501f922642d0dafa65693831a69492859cc9e2f
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-02-25 15:28:21 +0100

    formatted the code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Tools from './components/Tools';
-import Projects from './components/Projects';
-import Copy from './components/Copy';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Tools from "./components/Tools";
+import Projects from "./components/Projects";
+import Copy from "./components/Copy";
 
 function App() {
   return (
@@ -12,11 +12,10 @@ function App() {
       <Navbar />
       <Home />
       <About />
-      <Tools/>
-      <Projects/>
-      <Copy/>
-      </>
-
+      <Tools />
+      <Projects />
+      <Copy />
+    </>
   );
 }
 
