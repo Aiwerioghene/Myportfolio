7fd0d61fbf3c61c9735f39d0e83099ed26ebab4d
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-03-03 20:20:13 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="w-[100%]  h-[100vh] flex flex-col items-center justify-center text-gray-300">
       <Typed
         className="md:text-5xl lg:text-6xl sm:text-4xl text-2xl font-bold md:pl-4 pl-2 text-center font-fontt2 tracking-widest"
-        strings={["Front End Developer", "Welcome to my Portfolio!"]}
+        strings={["Front End Developer", "Welcome to my Portfolio :)"]}
         typeSpeed={120}
         backSpeed={100}
         loop
